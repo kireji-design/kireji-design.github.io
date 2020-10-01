be46e2e282127374f9ba18f509251b06698fb5bd
Alessia <71669712+kireji-design@users.noreply.github.com>
2020-10-01 09:13:36 +0200

    Final

--- js/hover.js
+++ js/hover.js
@@ -3,7 +3,7 @@ var tooltip = document.querySelectorAll('.coupontooltip');
 document.addEventListener('mousemove', fn, false);
 
 function fn(e) {
-    for (var i=tooltip.length; i--;) {
+    for (var i = tooltip.length; i--;) {
         tooltip[i].style.left = e.pageX + 'px';
         tooltip[i].style.top = e.pageY + 'px';
     }
