acf271d263684f5b6bc5b3745b5415e278e91cb2
Alessia Musio <44356102+alessiamusio@users.noreply.github.com>
2021-01-24 17:38:45 +0100

    New layout

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $(".menu-aperto").hide();
+    $(".show_hide").show();
+});
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+    $(".menu-aperto").animate({width: "toggle" });
+
+    
+    
+    
+    
+}
+
+
+
